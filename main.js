2663d27df2db2dab4b2eb3904c457fbeba48be30
DannieMo <danielowiri4zoe@gmail.com>
2023-01-27 17:43:09 +0100

    Add luxon.js, storage. js, worker.js files

--- main.js
+++ main.js
@@ -1,24 +1,115 @@
-// alert("Hellow world!");
-// console.log(alert);ction () {
-  if (titleInput.value == '' && authorInput.value == '') {
-    alert('Pls enter your input');
-  } else {
-    const bookListRack = document.createElement('newBooks');
+import time from "./modules/dateTime.js";
+import getBooks, { storeBook } from "./modules/storage.js";
 
-    const newTitle = document.createElement('newTitle');
-    newTitle.innerHTML = titleInput.value;
-    bookListRack.appendChild(newTitle);
-    const newAuthor = document.createElement('newTitle');
-    newAuthor.innerHTML = authorInput.value;
-    bookListRack.appendChild(newAuthor);
+/*=== Containers ===*/
+const books = document.querySelector(".mybooks");
+const form = document.querySelector(".form");
+const contacts = document.querySelector(".contacts");
 
-    awesomebooks.appendChild(bookListRack);
+/* Switch between sectionos */
+const renderSection = (e) => {
+  switch (e.target.id) {
+    case "contacts":
+      contacts.classList.remove("hidden");
+      books.classList.add("hidden");
+      form.classList.add("hidden");
+      return;
+
+    case "form":
+      contacts.classList.add("hidden");
+      books.classList.add("hidden");
+      form.classList.remove("hidden");
+      return;
+
+    case "mybooks":
+      contacts.classList.add("hidden");
+      form.classList.add("hidden");
+      books.classList.remove("hidden");
+      return;
+  }
+};
+
+const navBtns = document.querySelectorAll(".navitems");
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => renderSection(e));
+});
+
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const button = document.querySelector("#btn");
+const bookList = document.querySelector("#awesomebooks");
+
+const bookStore = getBooks();
+
+const addBook = (e, title, author, library) => {
+  e.preventDefault();
+  let updatedLib;
+  if (title !== "" && author !== "") {
+    library.push({ id: library.length + 1, title, author });
+    updatedLib = [...library];
+    storeBook(library);
+  }
+  return updatedLib;
+};
+
+const removeBook = (e, id, library) => {
+  e.preventDefault();
+
+  console.log(e, id, library);
+  let filter;
+  if (library.length) {
+    filter = library.filter((el) => el.id !== id);
+    console.log(filter);
+    storeBook(filter);
+    displayBook(getBooks(), bookList);
+  }
+  return filter;
+};
+
+const displayBook = (list, container) => {
+  container.innerText = "";
+  if (list.length) {
+    const ul = document.createElement("ul");
+    list.forEach((el) => {
+      const li = document.createElement("li");
+      const trash = document.createElement("span");
+      trash.innerHTML = `<i id=${el.id} class="fa-sharp fa-solid fa-trash"></i>`;
+      trash.addEventListener("click", (e) => {
+        removeBook(e, el.id, getBooks());
+        // displayTasks(listArray, container);
+      });
+      li.setAttribute("id", el.id);
+      li.innerHTML = `Author: ${el.author} Title: ${el.title}`;
+      li.append(trash);
+      ul.append(li);
+    });
+    container.append(ul);
+  }
+  return container;
+};
+
+const addStuff = (e) => {
+  addBook(e, titleInput.value, authorInput.value, bookStore);
+  displayBook(bookStore, bookList);
+  titleInput.value = "";
+  authorInput.value = "";
+  titleInput.focus();
+};
+
+button.addEventListener("click", (e) => {
+  addStuff(e);
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    addStuff(e);
   }
 });
-// Variables
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const button = document.querySelector('#btn');
-const bookList = document.querySelector('#awesomebooks');
 
-button.addEventListener('click',
+window.onload = () => {
+  contacts.classList.add("hidden");
+  books.classList.remove("hidden");
+  form.classList.add("hidden");
+  displayBook(bookStore, bookList);
+  time();
+};
---
+++ modules/storage.js
@@ -0,0 +1,16 @@
+/* ======== Stores a list to localstorage ============= */
+const storeBook = (items) => {
+  const list = JSON.stringify(items);
+  localStorage.setItem("bookshop", list);
+};
+
+/* ======== Gets a list from localstorage ============= */
+const getBooks = () => {
+  if (localStorage.getItem("bookshop")) {
+    return JSON.parse(localStorage.getItem("bookshop"));
+  }
+  return [];
+};
+
+export default getBooks;
+export { storeBook };
